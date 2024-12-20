da8d664de911dc19d7e724cacfdde48e59fa177a
Aarush Umap <aarush.umap@gmail.com>
2024-12-20 13:39:27 -0800

    init of multi-image stitcher

---
+++ image_stitcher.py
@@ -0,0 +1,12 @@
+class ImageStitcher():
+    def __init__(self):
+        pass
+
+    def load_data():
+        pass
+
+    def make_left_list():
+        pass
+
+    def make_right_list():
+        pass
